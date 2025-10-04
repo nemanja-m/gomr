ae6fd49895a83d78c140031d6516fea90a43ff0a
Nemanja Milicevic <the.nemanja.milicevic@gmail.com>
2025-10-04 10:35:25 +0200

    Configure job with CLI args

--- cmd/local/main.go
+++ cmd/local/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/nemanja-m/gomr/examples/wordcount"
@@ -11,10 +12,25 @@ import (
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
+	input := flag.String("input", "", "input files glob pattern")
+	output := flag.String("output", "", "output directory")
+	reducers := flag.Int("reducers", 4, "number of reducers (overrides default)")
+	flag.Parse()
+
+	if *input == "" {
+		log.Fatal("Input pattern must be specified using the -input flag")
+	}
+	if *output == "" {
+		log.Fatal("Output directory must be specified using the -output flag")
+	}
+	if *reducers <= 0 {
+		log.Fatal("Number of reducers must be >= 0")
+	}
+
 	config := core.JobConfig{
-		Input:       "examples/data/input/*.txt",
-		Output:      "examples/data/output/",
-		NumReducers: 4,
+		Input:       *input,
+		Output:      *output,
+		NumReducers: *reducers,
 		MapFunc:     wordcount.Map,
 		ReduceFunc:  wordcount.Reduce,
 	}
@@ -22,7 +38,7 @@ func main() {
 	engine := local.NewEngine(config)
 
 	log.Printf(
-		"Starting job with input: %s, output: %s, numReducers: %d",
+		"Starting job with input: %s, output: %s, reducers: %d",
 		config.Input,
 		config.Output,
 		config.NumReducers,
